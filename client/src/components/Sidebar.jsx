5e8fa3446e155729f6b9742b3515203d3f0b98cc
raghavroy2005 <raghav2005kumar@gmail.com>
2026-09-21 19:56:37 +0530

    updated sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom"
-import assets from "../assets/assets"
+import assets, { userDummyData } from "../assets/assets"
 
 
 const Sidebar = ({selectedUser,setSelectedUser}) => {
@@ -21,12 +21,33 @@ const Sidebar = ({selectedUser,setSelectedUser}) => {
                     </div>
                 </div>
             </div>
-            <div className="bg-[#282142] rounded-full flex items-center gap-2 py--3 px-4 mt-5">
+            <div className="bg-[#282142] rounded-full flex items-center gap-2 py-3 px-4 mt-5">
 
-                <img src={assets.search_icon} alt="Search" className="w-3" />
-                <input type="text" className="bg-transparent border-none outline-none
+                <img src={assets.search_icon} alt="Search" className="w-4" />
+                <input type="text" className="bg-transparent border-none outline-none w-5
                 text-white text-xs placeholder-[#c8c8c8] flex-1 placeholder='Search User...' " />
             </div>
+        </div>
+
+        <div className="flex flex-col">
+            {userDummyData.map((user,index)=>(
+                <div onClick={()=> {setSelectedUser}}
+                key={index}  className={`relative flex items-center gap-2 p-2 pl-4 rounded 
+                cursor-pointer max-sm:text-sm ${selectedUser?._id === user._id && 'bg-[#282142]/50'}`}>
+                    <img src={user?.profilePic || assets.avatar_icon} alt=""
+                    className="w-[35px] aspect-[1/1] rounde-full" />
+                    <div className="flex flex-col leading-5">
+                        <p>{user.fullName}</p>
+                        {
+                            index < 3 
+                            ? <span className="text-green-400 text-xs">Online</span>
+                            : <span className="text-neutral-400 text-xs">Offline</span>
+                        }
+                    </div>
+                    {index > 2 && <p className="absolute top-400 right-4 text-xs h-5 w-5
+                    flex justify-center items-center rounded-full bg-violate-500/50">{index}</p>}
+                </div>
+            ))}
         </div>
     </div>
   )
