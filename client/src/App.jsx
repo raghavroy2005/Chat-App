123a1b5d7d6e740cb82ef939cb2ae743b2b127fe
raghavroy2005 <raghav2005kumar@gmail.com>
2026-09-22 20:47:32 +0530

    we have update the side bare

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import { Routes , Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage"
 import LoginPage from "./pages/LoginPage"
 import ProfilePage from "./pages/ProfilePage"
-
+import bgImage from "./assets/bgImage.svg"
 
 const App = () => {
   return (
-    <>
-      <div className="bg-blue-300">
-        <Routes>
-          <Route  path='/' element={<HomePage/>} />
-          <Route path='/login' element={<LoginPage/>} />
-          <Route path='profile' element={<ProfilePage/>} />
-        </Routes>
-      </div>
-    </>
+    <div
+      className="min-h-screen w-full bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </div>
   )
 }
 
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -8,13 +8,15 @@ const HomePage = () => {
 
     const [selectedUser,setSelectedUser ] = useState(false)
   return (
-    <div className="border w-full h-screen">
+    <div className="border w-full h-screen sm:px-[5%] sm:py-[5%] bg-contain">
        <div className={`backdrop-blue-xl border-2 border-gray-600 rounded-2xl overflow-hidden
-       h-full grid grid-cols-1 relative ${selectedUser ?
+       h-100% grid grid-cols-1 relative ${selectedUser ?
        'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]':'md:grid-cols-2'}`}>
-            <Sidebar/>
-            <ChatContainer/>
-            <RightSidebar/>
+
+          <Sidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+          <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+          <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser} />
+
        </div>
     </div>
   )
